43e4b46e52e3390d8f6ddd30084bb83543ed8fb0
shashankack <shashank.ac.dev@gmail.com>
2024-04-07 14:26:11 +0530

    Docker Containers Created

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 # App to Manage NSS Events
 
 ## STACK - Django Backend, React Frontend, Postgres DB
+
+## Local Development
+
+### Build Images
+run the command from root - 
+```docker-compose build```
+
+### Run
+```docker-compose up -d```
+This will start 3 containers:
+- API container on port 8000
+- UI container on port 3000
+- DB container on port 5432
+
+### View containers
+```docker-compose ps```
+
+### Stop containers
+```docker-compose down```
+
+### Restart containers
+```docker-compose restart```
+
--- backend/api/settings.py
+++ backend/api/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB') or 'postgres',
+        'USER': os.environ.get('POSTGRES_USER') or 'postgres',
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD') or 'password',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
