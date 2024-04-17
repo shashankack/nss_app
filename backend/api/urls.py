70a82483f1993fc7e297e3b890b1ddd41e5dbd40
shashankack <shashank.ac.dev@gmail.com>
2024-04-17 18:12:16 +0530

    NULL

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -15,12 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-
-import nss_profile
-
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('nss_profile.urls')),
 ]
--- backend/nss_profile/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Address, User
-
-# Register your models here.
-
-admin.site.register(Address)
-admin.site.register(User)
