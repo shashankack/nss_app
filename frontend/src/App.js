be4b6afb298b9a41a0a00bbb6c72f20ea6c7da6c
CHATUR-BHARADWAJ-098 <applesoft913@gmail.com>
2024-05-11 21:26:22 +0530

    login update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,17 @@
-// app.js
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './Routes';
-import Navbar from './components/Navbar'; // Import Navbar component
-
-function App() {
-
-  
+import Navbar from './components/Navbar'; 
+function App() { 
   return (
+
     <Router>
       <div className="App">
         <Navbar />
-        <Routes /> {/* Render the Routes component */}
+        <Routes /> 
       </div>
     </Router>
+
   );
 }
 
