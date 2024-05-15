7b425233b9135dff96a4f87e579374ff78f7f8a2
Shashank <166240810+shashankack@users.noreply.github.com>
2024-05-16 00:01:00 +0530

    Delete backend directory

--- backend/nss_profile/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import User, VolunteerProfile, College, CollegeCourses
-
-# Register your models here.
-
-admin.site.register(User)
-admin.site.register(VolunteerProfile)
-admin.site.register(College)
-admin.site.register(CollegeCourses)
