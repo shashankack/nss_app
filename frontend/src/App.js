7561240f6dd78bb68140186d0ee20f3c2b39c6d0
CHATUR-BHARADWAJ-098 <applesoft913@gmail.com>
2024-05-28 20:15:16 +0530

    hello

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Route, BrowserRouter as Router,Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Routes from './Routes'; 
+import NewRoutes from './NewRoutes'; 
 import PrivateRoute from './components/PrivateRoute';
 function App() {
   return (
       <div className='App'>
-        <Routes /> 
+        <NewRoutes /> 
       </div>
   )
 }
