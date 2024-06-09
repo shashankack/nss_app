6adc5c75ccad8b5d72d49c97a23d100a5584af01
shashankack <shashank.ac.dev@gmail.com>
2024-06-09 21:22:51 +0530

    Fixed Navbar Issue

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,94 +1,183 @@
-// Login.js
-import React, { useState } from 'react';
-import { Container, Typography, TextField, Button, Grid } from '@mui/material';
-import {makeStyles} from '@mui/styles'
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
+import {  useLocation, useNavigate } from 'react-router-dom';
+import api from '../utils/api'
+import { clearTokens } from '../utils/auth'
 import axios from 'axios';
-import { setAccessToken, setRefreshToken } from '../utils/auth'
-import { useNavigate  } from 'react-router-dom';
-
-const useStyles = makeStyles((theme) => ({
-
-}));
- 
-const Login = () => {
-const classes = useStyles();
-const [email, setEmail] = useState('');
-const [password, setPassword] = useState('');
-const [error, setError] = useState('');
-const nav = useNavigate();
 
 
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
+
+function ResponsiveAppBar() {
+  const location = useLocation();
+  const isNotLoginPage = location.pathname != '/login';
+  const nav = useNavigate();
+    
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const [firstName, setFirstName] = React.useState('');
+  const [userProfile, setUserProfile] = React.useState({});
+
+  React.useEffect(() => {
+    if (isNotLoginPage) {
+      api.get('/loggedInUserProfile/')
+      .then (response => {
+        setUserProfile(response.data);
+        console.log(userProfile)
+      })
+      .catch(error => {
+        console.error();
+      });
+    }
+  }, [isNotLoginPage]);
+
+  const handleOpenNavMenu = (event) => {
+    console.log(userProfile)
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
   };
 
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
   };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(email);
-    console.log(password);
-    axios.post(`http://localhost:8000/api/login/`, { "email": email, "password": password })
-    .then((response) => {
-        setAccessToken(response.data.access);
-        setRefreshToken(response.data.refresh);
-        nav('/');
-
-    })
-    .catch((error) => {
-        setError('Invalid login credentials');
-    });
-
+  const handleLogout = () => {
+    handleCloseNavMenu();
+    handleCloseUserMenu();
+    clearTokens();
+    nav("/login");
+  }
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
   };
-
 
   return (
-    <Container component="main" maxWidth="xs">
-      <div>
-        <Typography component="h1" variant="h5">
-          Sign in
-        </Typography>
-        <form className={classes.form} onSubmit={handleSubmit}>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-             autoComplete="email"
-             autoFocus
-            onChange={handleEmailChange}
-            />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-            onChange={handlePasswordChange}
-            />
-            {error && <Typography color="error">{error}</Typography>}
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
+    isNotLoginPage && 
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
           >
-            Sign In
-          </Button>
-        </form>
-      </div>
-    </Container>
+              {userProfile.college}
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+                <MenuItem key="name" onLoad={setFirstName}>
+                  <Typography textAlign="center">Welcome, {firstName}</Typography>
+                </MenuItem>
+            </Menu>
+          </Box>
+          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            LOGO
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+                <MenuItem key="profile" onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">My Profile</Typography>
+                </MenuItem>
+                <MenuItem key="logout" onClick={handleLogout}>
+                  <Typography textAlign="center">Logout</Typography>
+                </MenuItem>
+
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 }
-
-export default Login;
+export default ResponsiveAppBar;
