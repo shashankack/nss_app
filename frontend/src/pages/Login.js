9dc5f8896c4ecac2c20f55ba8401e87a04bc1ae3
shashankack <shashank.ac.dev@gmail.com>
2024-06-23 05:44:30 +0000

    Enhanced Login Page

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,22 +1,47 @@
-// Login.js
 import React, { useState } from 'react';
-import { Container, Typography, TextField, Button, Grid } from '@mui/material';
-import {makeStyles} from '@mui/styles'
+import { Container, Typography, TextField, Button, Box } from '@mui/material';
+import { makeStyles } from '@mui/styles';
 import axios from 'axios';
-import { setAccessToken, setRefreshToken } from '../utils/auth'
-import { useNavigate  } from 'react-router-dom';
+import { setAccessToken, setRefreshToken } from '../utils/auth';
+import { useNavigate } from 'react-router-dom';
+import logo from "../assets/nss_logo.png"
 
-const useStyles = makeStyles((theme) => ({
-
+const useStyles = makeStyles(() => ({
+  root: {
+    display: 'flex',
+    margin: 'auto',
+    marginTop: '64px',
+    borderRadius: '4px',
+    width: '900px',
+    height: '600px',
+    padding: '5px',
+    boxShadow: '4px 15px 150px rgba(0, 0, 0, 0.2)',
+  },
+  formContainer: {
+    marginTop: '60px',
+    maxWidth: 'xs',
+  },
+  form: {
+    width: '100%',
+  },
+  submit: {
+    marginTop: '16px',
+    width: '200px',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    objectFit: 'cover',
+    borderRadius: '4px 0 0 4px',
+  },
 }));
- 
-const Login = () => {
-const classes = useStyles();
-const [email, setEmail] = useState('');
-const [password, setPassword] = useState('');
-const [error, setError] = useState('');
-const nav = useNavigate();
 
+const Login = () => {
+  const classes = useStyles();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
 
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
@@ -28,67 +53,68 @@ const nav = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(email);
-    console.log(password);
-    axios.post(`http://localhost:8000/api/login/`, { "email": email, "password": password })
-    .then((response) => {
+    axios.post('http://localhost:8000/api/login/', { email, password })
+      .then((response) => {
         setAccessToken(response.data.access);
         setRefreshToken(response.data.refresh);
-        nav('/');
-
-    })
-    .catch((error) => {
+        navigate('/');
+      })
+      .catch(() => {
         setError('Invalid login credentials');
-    });
-
+      });
   };
 
-
   return (
-    <Container component="main" maxWidth="xs">
-      <div>
-        <Typography component="h1" variant="h5">
-          Sign in
-        </Typography>
-        <form className={classes.form} onSubmit={handleSubmit}>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-             autoComplete="email"
-             autoFocus
-            onChange={handleEmailChange}
+    <Box className={classes.root} sx={{mt:16}}>
+      <Box sx={{ flex: 1, mt:15, ml:6, mr:-10 }}>
+        <img src={logo} alt="Login" width={350}/>
+      </Box>
+      <Box sx={{ flex: 1, display: 'flex', justifyContent: 'center', alignItems: 'center', mt:-4, mr:4 }}>
+        <Container className={classes.formContainer}>
+          <Typography component="h1" variant="h4">
+            Sign in
+          </Typography>
+          <form className={classes.form} onSubmit={handleSubmit}>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              onChange={handleEmailChange}
             />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-            onChange={handlePasswordChange}
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              onChange={handlePasswordChange}
             />
             {error && <Typography color="error">{error}</Typography>}
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Sign In
-          </Button>
-        </form>
-      </div>
-    </Container>
+            <Box display="flex" justifyContent="center">
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+              >
+                Sign In
+              </Button>
+            </Box>
+          </form>
+        </Container>
+      </Box>
+    </Box>
   );
-}
+};
 
 export default Login;
